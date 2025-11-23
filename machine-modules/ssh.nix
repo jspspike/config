2c591274cfc2bba60d2f60ab9b99d0b1491728b6
jspspike <jspspike@gmail.com>
2025-11-23 14:19:03 -0600

    Add desktop public key for ssh

--- machine-modules/ssh.nix
+++ machine-modules/ssh.nix
@@ -11,6 +11,7 @@
   };
 
   users.users.jspspike.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtVtM+mU2f+TVHYfgCnS6BGLa+AnTOenriWcrNAgBv3 jspspike@gmail.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtVtM+mU2f+TVHYfgCnS6BGLa+AnTOenriWcrNAgBv3 jspspike@gmail.com" # laptop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJI7PjE1fNRCW2jQacMFiaIDDrjLgYNV4NAWToedNQOz jspspike@gmail.com" # desktop
   ];
 }
