f39a7fb673468ff300971597de7f7c90a12b82bc
jspspike <jspspike@gmail.com>
2026-01-07 20:37:01 -0600

    Switch media local ip

--- hosts/media/dns.nix
+++ hosts/media/dns.nix
@@ -5,11 +5,11 @@
     enable = true;
 
     settings = {
-      listen-address = ["192.168.1.117" "127.0.0.1"];
+      listen-address = ["192.168.0.117" "127.0.0.1"];
 
       server = ["1.1.1.1"];
 
-      address = [ "/home.weewoo.dev/192.168.1.117" "/photos.weewoo.dev/192.168.1.117" ];
+      address = [ "/home.weewoo.dev/192.168.0.117" "/photos.weewoo.dev/192.168.0.117" ];
 
       no-resolv = true;
       resolv-file = "/etc/dnsmasq-resolv.conf";
